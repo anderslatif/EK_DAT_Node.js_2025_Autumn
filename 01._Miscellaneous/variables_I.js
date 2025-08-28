dd131c35e0b12a520aed2e233e958ac099e90b41
anderslatif <anderslatif@users.noreply.github.com>
2025-08-28 10:55:34 +0200

    Created the variables and person files

---
+++ 01._Miscellaneous/variables_I.js
@@ -0,0 +1,28 @@
+// node <file_name>
+
+// use const if possible
+// otherwise use let
+
+const name = "Oliver";
+
+console.log(`Hello ${2 + 2}`);
+
+// string literals
+
+const firstGreeting = 'It is a nice day " \' today';
+const secondGreeting = "It's a nice day today"; 
+const thirdGreeting = `It's a nice " day
+    multi-line comment
+today`;
+
+// String, Boolean, Number, BigInt, Object, Symbol, undefined, null
+
+// Javascript Object
+const person = {
+    name: "Jens", // key-value pair
+};
+
+
+
+
+
