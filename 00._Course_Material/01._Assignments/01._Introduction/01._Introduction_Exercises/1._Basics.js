ce71d4a78a96376db3d2504c59fb1b0a53695612
anderslatif <anderslatif@users.noreply.github.com>
2025-08-28 11:36:24 +0200

    Fixed the link in the semester plan

--- 00._Course_Material/01._Assignments/01._Introduction/01._Introduction_Exercises/1._Basics.js
+++ 00._Course_Material/01._Assignments/01._Introduction/01._Introduction_Exercises/1._Basics.js
@@ -9,6 +9,8 @@ const lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 
+console.log(`My first name is ${firstName} and my last name is ${lastName}`);
+
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -19,6 +21,21 @@ const increment = 1;
 // Add the year plus the increment
 // The result should be 2025
 // You cannot touch the first or the second line
+
+const currentYearFirst = Number(year) + increment;
+const currentYearSecond = parseInt(year) + increment;
+const currentYearThird = +year + increment;
+
+console.log(currentYearFirst);
+console.log(currentYearSecond);
+console.log(currentYearThird);
+
+const possibleNumber = "01235adsf8";
+
+console.log(Number(possibleNumber));
+console.log(parseInt(possibleNumber));
+console.log(+possibleNumber);
+
 
 
 // --------------------------------------
