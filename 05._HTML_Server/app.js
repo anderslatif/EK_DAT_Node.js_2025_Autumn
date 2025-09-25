2f59fe86638ca6735297bddeec84ee9f113479fc
anderslatif <anderslatif@users.noreply.github.com>
2025-09-25 08:42:56 +0200

    Showed how to serve as static files

--- 05._HTML_Server/app.js
+++ 05._HTML_Server/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/public/frontend/index.html");
 });
 
 let visitorsCount = 0;
