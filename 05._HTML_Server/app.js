924eb02fd7b8de448f5ae089b2947238f87abcf8
anderslatif <anderslatif@users.noreply.github.com>
2025-09-25 10:52:23 +0200

    Finished the redirection topic

--- 05._HTML_Server/app.js
+++ 05._HTML_Server/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 const app = express();
 
+import path from "path";
+
 app.use(express.static("public"));
 
 // const candyCommon = require("./util/candyCommon.js");
@@ -8,17 +10,21 @@ app.use(express.static("public"));
 import candyESModule from "./util/candyESModule.js";
 console.log(candyESModule);
 
-
+// console.log(path.resolve("public/redirection/redirection.html"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/frontend/index.html");
+    res.sendFile(path.resolve("public/frontend/index.html"));
 });
 
+
 app.get("/candy", (req, res) => {
-    res.sendFile(__dirname + "/public/candy/candy.html");
+    res.sendFile(path.resolve("public/candy/candy.html"));
 });
 
 // task create a /redirection route and page called redirection.html
+app.get("/redirection", (req, res) => {
+    res.sendFile(path.resolve("public/redirection/redirection.html"));
+});
 
 
 let visitorsCount = 0;
@@ -27,6 +33,9 @@ app.get("/visitorscount", (req, res) => {
     res.send({ data: ++visitorsCount });
 });
 
+app.get("/visitorsdoorway", (req, res) => {
+    res.redirect("/visitorscount");
+});
 
 const PORT = 8080;
 app.listen(PORT, () => {
