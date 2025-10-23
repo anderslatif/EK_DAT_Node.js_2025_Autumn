42501328cc7cb62eab0c32d1892d8dd97adec7e7
anderslatif <anderslatif@users.noreply.github.com>
2025-10-23 11:02:36 +0200

    Finished the asynchronous lesson

--- 01._Miscellaneous/asynchronous.js
+++ 01._Miscellaneous/asynchronous.js
@@ -13,19 +13,25 @@
 // pending => fulfilled
             // resolved | rejected
 
+// Problem: Can end up with nested promises
 
-new Promise((resolve, reject) => {
-    setTimeout(() => {
-        try {
-            throw "Oh no!";
-            resolve("Everything went well");
-        } catch (error) {
-            reject(error);
-        }
-    }, 2000);
-})
-.then((message) => console.log(message))
-.catch((errorMessage) => console.log(errorMessage));
+// Solution 3: Async/Await (Syntactic sugar for promises)
+
+// Problem: Must add try/catch blocks to handle errors
+
+
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         try {
+//             throw "Oh no!";
+//             resolve("Everything went well");
+//         } catch (error) {
+//             reject(error);
+//         }
+//     }, 2000);
+// })
+// .then((message) => console.log(message))
+// .catch((errorMessage) => console.log(errorMessage));
 
 
 /* Assignment: Create a promisified function (called myPromise) 
@@ -34,3 +40,56 @@ Make it resolve as "Something Good" and/or reject as "Something Bad"
 create a 3 second delay to simulate asynchronous behavior
 */
 
+function myPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try {
+                throw new Error("Something Bad");
+                resolve("Something Good");
+            } catch (error) {
+                reject(error);
+            }
+        }, 3000)
+    });
+}
+
+/* myPromise()
+.then((successMessage) => console.log(successMessage))
+.catch((errorMessage) => console.log(errorMessage));
+ */
+
+
+// try {
+//     const successMessage = await myPromise();
+//     console.log(successMessage);
+// } catch (error) {
+//     console.log(error);
+// }
+
+/* assignment
+    try to simulate the fetch function
+    call it myFetch
+    it should return the promise json() after 2.5 seconds
+    so that you can call response.json() on it
+    as much as possible try to imagine how fetch works and simulate the underlying code
+*/
+function myFetch(URL, options) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                json: () => new Promise((resolve, reject) => resolve("Data from the server"))
+            });
+        }, 2500);
+    });
+}
+
+// myFetch("somewebsite.com")
+// .then((result) => result.json())
+// .then((response) => console.log(response));
+
+// IIFE
+(async function () {
+    const result = await myFetch("somewebsite.com");
+    const response = await result.json();
+    console.log(response);
+})()
