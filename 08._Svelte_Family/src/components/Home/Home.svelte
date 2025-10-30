d21ed9e8d2771c1dd373523e0e334ad3018696d9
anderslatif <anderslatif@users.noreply.github.com>
2025-10-30 09:41:36 +0100

    Created the parent component

---
+++ 08._Svelte_Family/src/components/Home/Home.svelte
@@ -0,0 +1,14 @@
+
+<slot></slot>
+
+<h2 id="welcome-mat">Velkommen til måtten</h2>
+
+
+<style>
+    #welcome-mat {
+        color: black;
+        background-color: azure;
+        border-radius: 0 0 2em 2em;
+        padding: 2em;
+    }
+</style>
---
+++ 08._Svelte_Family/src/components/Parent/Parent.svelte
@@ -0,0 +1,9 @@
+<script>
+    const { parentName, children } = $props();
+</script>
+
+<h2>{parentName}</h2>
+
+{#each children as child (child.name)}
+    <h3>{child.name}</h3>
+{/each}
