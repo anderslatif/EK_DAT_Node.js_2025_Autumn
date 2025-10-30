83aaafcc5687615a29f6e94091630012398b07f3
anderslatif <anderslatif@users.noreply.github.com>
2025-10-30 10:46:56 +0100

    Finished handleShowLove / onShowLove

---
+++ 08._Svelte_Family/src/components/Child/Child.svelte
@@ -0,0 +1,36 @@
+<script>
+    const { name, familySheep, isGirl, onShowLove } = $props();
+</script>
+
+<div
+    class={familySheep || "not-a-sheep"}
+    class:is-girl={isGirl}
+    class:is-boy={!isGirl}
+>
+    <h3>{name}</h3>
+</div>
+
+<button onclick={() => onShowLove(name)}>Show your parent some love</button>
+
+
+<style>
+    .black-sheep {
+        background-color: crimson;
+    }
+
+    .grey-sheep {
+        background-color: navajowhite;
+    }
+
+    .not-a-sheep {
+        background-color: yellowgreen;
+    }
+
+    .is-girl {
+        border: 0.5em solid rebeccapurple;
+    }
+
+    .is-boy {
+        border: 0.5em solid cadetblue;
+    }
+</style>
--- 08._Svelte_Family/src/components/Parent/Parent.svelte
+++ 08._Svelte_Family/src/components/Parent/Parent.svelte
@@ -1,9 +1,36 @@
 <script>
+    import Child from "../Child/Child.svelte";
+
     const { parentName, children } = $props();
+
+    const loveHistory = $state([
+        { 
+            name: "self-love",
+            love: "💞"
+    }]);
+
+    function handleShowLove(childsName) {
+        loveHistory.push({
+            name: childsName,
+            love: "💞"
+        });
+    }
+
+    const cookieJar = $state(["🍪", "🍪", "🍪", "🍪", "🍪"]);
+
+    /* 
+    assignment: allow children to eat a cookie from the jar
+    bonus: if the jar is empty, let the parent fill it
+ */
 </script>
 
 <h2>{parentName}</h2>
 
+{#each loveHistory as love}
+    <span>{love.name}: {love.love}</span>   
+{/each}
+
+
 {#each children as child (child.name)}
-    <h3>{child.name}</h3>
+    <Child {...child} onShowLove={handleShowLove} />
 {/each}
