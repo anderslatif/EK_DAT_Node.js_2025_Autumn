9e5d543f42492d5ad253a0d601bbef39dd42f329
anderslatif <anderslatif@users.noreply.github.com>
2025-11-06 10:22:52 +0100

    Added rate limiting to the server

--- 09._Middleware_Security/app.js
+++ 09._Middleware_Security/app.js
@@ -1,6 +1,28 @@
 import express from 'express';
 const app = express();
 
+import { rateLimit } from 'express-rate-limit'
+
+const generalLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+	// store: ... , // Redis, Memcached, etc. See below.
+});
+
+app.use(generalLimiter);
+
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 3,
+    standardHeaders: 'draft-8',
+    legacyHeaders: false
+});
+
+app.use("/auth", authLimiter);
+
 
 import authRouter from './routers/authRouter.js';
 app.use(authRouter);
--- 09._Middleware_Security/routers/authRouter.js
+++ 09._Middleware_Security/routers/authRouter.js
@@ -2,11 +2,29 @@ import { Router } from 'express';
 
 const router = Router();
 
-function isAdmin() {
-    
+/* 
+    Authentication: Trying to establish identity. 
+        E.g. A login where a user uses a username and password to authenticate themselves
+
+    Ahtorhizataion: Trying to establish if the user is allowed to access the resources.
+        E.g. Only an admin user is allowed to access the route. We need to check if they are admin or not.
+*/
+
+function isAdmin(req, res, next) {
+    // this simulates checking in the database
+    // and assumes that the user is admin
+    const isAdmin = true;
+    if (isAdmin) {
+        req.isAdmin = isAdmin;
+        // again, simulates username from the database
+        req.username = "Johnnie";
+        return next();
+    }
+    res.status(403).send({ data: "You need to be an admin to access this route" });
 }
 
-router.get("/secretroute", (req, res) => {
+router.get("/auth/secretroute", isAdmin, (req, res) => {
+    console.log(req.isAdmin, req.username);
     res.send({ data: "Some secret data" });
 });
 
