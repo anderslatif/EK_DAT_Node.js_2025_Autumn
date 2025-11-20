3fd0b81e13a2b1bc15169d34e6331d76527b18f9
anderslatif <anderslatif@users.noreply.github.com>
2025-11-20 10:51:27 +0100

    Finished the routers

--- 11._SQL_Fitness/app.js
+++ 11._SQL_Fitness/app.js
@@ -1,6 +1,13 @@
-import { express } from 'express';
+import express from 'express';
 
 const app = express();
+
+app.use(express.json());
+
+import exercisesRouter from './routers/exercisesRouter.js';
+app.use(exercisesRouter);
+import usersRouter from './routers/usersRouter.js';
+app.use(usersRouter);
 
 
 
