032fc49d395d8f40da309bcd2c44fcfc67f49b20
anderslatif <anderslatif@users.noreply.github.com>
2025-12-04 09:04:27 +0100

    Setup the socket server in conjunction with Express

---
+++ 13._Sockets_Plain/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+const app = express();
+
+app.use(express.static("public"));
+
+import http from 'http';
+const server = http.createServer(app);
+
+import { Server } from 'socket.io';
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("A socket connected", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("Socket disconnected", socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log("Server is running on port", PORT));
